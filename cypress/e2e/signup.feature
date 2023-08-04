cc2d019b25c729b94587382c2ba75a1769a7fe2c
Swati Kamboj <swati@netwealth.com.au>
2023-08-04 17:54:33 +1000

    Iteration 1 working on Production Form

--- cypress/e2e/signup.feature
+++ cypress/e2e/signup.feature
@@ -6,10 +6,12 @@ Feature: Sign Up a Customer
 
 Scenario: Successfull Customer Sign Up with valid details
 Given I am on the sign up page
-When I fill in the valid form details:
+
+When I fill in the valid form details from below:
+    | Customer Name | Registration No | StreetAddress1 | StreetAddress2 | StreetAddress3 | City | State| Country | Postcode | Main Phone | Fax | Website | Email | Source | Industry | FirstName | LastName | Direct Phone | Job Title | PrimaryEmail |
+    | Swati | 123456 | 22 | Monomeath Drive | Mickleham | Melbourne | VIC | Fiji | 3064 | 0412121212 | 1122334455 | www.test.com | test@test.com | Web Search | IT Consultants | Swati | Kamboj | 0412131415 | CIO | test@crayon.com |
     
-    | Customer Name | Registration No | StreetAddress1 | Postcode |
-    | Swati | 123456 | 22 Monomeath Drive | 3064 |
-    
+And Click on the Sign Up button
+
 Then I should see a success message for customer signed up
 
--- cypress/e2e/signup/signup.js
+++ cypress/e2e/signup/signup.js
@@ -1,23 +1,42 @@
-import { Given, When, Then} from "cypress-cucumber-preprocessor/steps";
+import { Given, When, And, Then } from "cypress-cucumber-preprocessor/steps";
 
-Given("I am on the sign up page", ()=>{
-  cy.visit("https://www.rhipe.com/sign-me-up/")
+Given("I am on the sign up page", () => {
+  cy.visit("https://www.rhipe.com/sign-me-up/");
   //cy.get(".entry-title").should('have.text',"Sign up with Rhipe")
+});
 
-})
+When("I fill in the valid form details from below:", (customerInfoTable) => {
+  customerInfoTable.hashes().forEach((customerEntry) => {
+    cy.log(customerEntry["Customer Name"]);
+    cy.get("#CustomerName").type(customerEntry["Customer Name"]);
+    cy.get("#RegistrationNumber").type(customerEntry["Registration No"]);
+    cy.get("#Street1").type(customerEntry["StreetAddress1"]);
+    cy.get("#Street2").type(customerEntry["StreetAddress2"]);
+    cy.get("#Street3").type(customerEntry["StreetAddress3"]);
+    cy.get("#City").type(customerEntry["City"]);
+    cy.get("#State").type(customerEntry["State"]);
+    cy.get("#Country").select(customerEntry["Country"]);
+    cy.get("#Postcode").type(customerEntry["Postcode"]);
+    cy.get("#MainPhone").type(customerEntry["Main Phone"]);
+    cy.get("#Fax").type(customerEntry["Fax"]);
+    cy.get("#WebUrl").type(customerEntry["Website"]);
+    cy.get("#Email").type(customerEntry["Email"]);
+    cy.get("#Source").select(customerEntry["Source"]);
+    cy.get("#IndustryType").select(customerEntry["Industry"]);
 
-When("I fill in the valid form details:", (customerInfoTable)=>{
-  
-  customerInfoTable.hashes().forEach(customerEntry => {
-    cy.log(customerEntry["Customer Name"])
-    cy.get('#CustomerName').type(customerEntry["Customer Name"])
-    cy.get('#RegistrationNumber').type(customerEntry["Registration No"])
-    cy.get('#Street1').type(customerEntry["StreetAddress1"])
-    cy.get('#Postcode').type(customerEntry["Postcode"])
+    cy.get("#PrimaryContactFirstName").type(customerEntry["FirstName"]);
+    cy.get("#PrimaryContactLastName").type(customerEntry["LastName"]);
+    cy.get("#PrimaryContactPhone").type(customerEntry["Direct Phone"]);
+    cy.get("#PrimaryContactJobTitle").type(customerEntry["Job Title"]);
+    cy.get("#PrimaryContactEmail").type(customerEntry["PrimaryEmail"]);
   });
-  
-})
+});
 
-Then("I should see a success message for customer signed up",()=>{
-    
-})
+And("Click on the Sign Up button", () => {
+  //SK - Skipping the click of Sign Up button as per the case study requirement
+  cy.get("#submitbutton");
+});
+
+Then("I should see a success message for customer signed up", () => {
+  //SK - Here we can put an assertion to see a success message or Title of next page
+});
