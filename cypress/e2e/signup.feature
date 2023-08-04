1d3f5d367dceb79b06c8b7d1cd9097009232bc6c
Swati Kamboj <swati@netwealth.com.au>
2023-08-04 16:18:48 +1000

    CustomerEntry working via DataTable in feature file

--- cypress/e2e/signup.feature
+++ cypress/e2e/signup.feature
@@ -6,6 +6,10 @@ Feature: Sign Up a Customer
 
 Scenario: Successfull Customer Sign Up with valid details
 Given I am on the sign up page
-When I fill in the form details
+When I fill in the valid form details:
+    
+    | Customer Name | Registration No | StreetAddress1 | Postcode |
+    | Swati | 123456 | 22 Monomeath Drive | 3064 |
+    
 Then I should see a success message for customer signed up
 
--- cypress/e2e/signup/signup.js
+++ cypress/e2e/signup/signup.js
@@ -1,14 +1,23 @@
 import { Given, When, Then} from "cypress-cucumber-preprocessor/steps";
 
 Given("I am on the sign up page", ()=>{
-  cy.visit("https://dev-rhipe-signup.azurewebsites.net/ ")
+  cy.visit("https://www.rhipe.com/sign-me-up/")
+  //cy.get(".entry-title").should('have.text',"Sign up with Rhipe")
 
 })
 
-When("I fill in the form details", ()=>{
-    cy.get(".entry-title").should('have.text',"Sign up with Rhipe")
+When("I fill in the valid form details:", (customerInfoTable)=>{
+  
+  customerInfoTable.hashes().forEach(customerEntry => {
+    cy.log(customerEntry["Customer Name"])
+    cy.get('#CustomerName').type(customerEntry["Customer Name"])
+    cy.get('#RegistrationNumber').type(customerEntry["Registration No"])
+    cy.get('#Street1').type(customerEntry["StreetAddress1"])
+    cy.get('#Postcode').type(customerEntry["Postcode"])
+  });
+  
 })
 
 Then("I should see a success message for customer signed up",()=>{
-    cy.get(".entry-title").should('have.text',"Sign up with Rhipe")
+    
 })
